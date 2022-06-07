178bfdd12223fd5e955726b56797a9bedbcca008
Daria Eremina <eremina.daria@gmail.com>
2022-06-07 12:53:43 +0300

    delete commented code

--- src/test/java/site/stellarburgers/tests/GoToProfileTest.java
+++ src/test/java/site/stellarburgers/tests/GoToProfileTest.java
@@ -1,34 +1,13 @@
 package site.stellarburgers.tests;
 
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 import site.stellarburgers.pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.junit.Assert.assertEquals;
 
 public class GoToProfileTest {
-
-        /*
-    ChromeDriver driver;
-
-    @Before
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "/Users/daraeremina/Downloads/yandexdriver");
-        driver = new ChromeDriver();
-        setWebDriver(driver);
-    }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
-     */
 
     @Test
     public void goToProfilePositiveTest() {
